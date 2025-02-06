4cabf3e165a436e4f4eaa993a9aaaafac2deb07d
luchob89 <luchob89@gmail.com>
2025-02-06 15:52:43 -0300

    Icons and opengraph fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -11,6 +12,11 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
+
+export const metadata: Metadata = {
+ title: "Dolar MEP React/Next.js App",
+ description: "Technical test for Latin Securities Company",
+};
 
 export default function RootLayout({
   children,
